c5c26e575d9775af2dc72913de6a5af52d859747
cczball <113612926+cczball@users.noreply.github.com>
2022-09-16 15:22:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ export const config = {
     // 所在省份
     province: "福建",
     // 所在城市
-    city: ["漳州","莆田","漳州"],
+    city: "漳州",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "曾曾", "year": "1998", "date": "06-02", "type": 'new'},
